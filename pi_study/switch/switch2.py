d9d7b18a20f218814ee88b76d6e770fbc6d033c3
VRSoda <jun.codework@gmail.com>
2025-05-08 16:16:04 +0900

    g

---
+++ pi_study/switch/switch2.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as gpio
+from time import sleep
+
+swPin = [13,19]
+text = [
+        "1번text",
+        "2번text"
+        ]
+gpio.setmode(gpio.BCM)
+
+for i in swPin:
+    gpio.setup(swPin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+def btncheck():
+    for i in range(len(swPin)):
+        pin = swPin[i]
+
+        if gpio.input(pin) == gpio.HIGH:
+            print(f"{i+1}버튼을 눌렀습니다")
+            if i == 0:
+                print(text[0])
+            elif i == 1:
+                print(text[1])
+            sleep(0.5)
+            while gpio.input(pin) == gpio.HIGH:
+    while True:
+        btncheck()
+except KeyboardInterrupt:
+    pass
+finally:
+    gpio.cleanup()
---
+++ pi_study/switch/switch3.py
@@ -0,0 +1,73 @@
+import RPi.GPIO as gpio
+from time import sleep
+
+gpio.setmode(gpio.BCM)
+
+
+class Led:
+
+    def __init__(self, pin, color):
+        self.pin = pin
+        self.color = color
+        gpio.setup(pin, gpio.OUT)
+        gpio.output(pin, gpio.LOW)
+
+    def blink(self, count, time):
+        for _ in range(count):
+            gpio.output(self.pin, gpio.HIGH)
+            sleep(time)
+            gpio.output(self.pin, gpio.LOW)
+            sleep(time)
+
+    def ledOn(self):
+        gpio.output(self.pin, gpio.HIGH)
+
+    def ledOff(self):
+        gpio.output(self.pin, gpio.LOW)
+
+
+class Button:
+
+    def __init__(self, pin, onPressed):
+        self.pin = pin
+        self.prevState = gpio.LOW
+        self.onPressed = onPressed
+        gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+    def waitPressed(self):
+        currentState = gpio.input(self.pin)
+        if self.checkPressed(currentState):
+            self.onPressed()
+        self.prevState = currentState
+        sleep(0.05)
+
+    def checkPressed(self, currentState):
+        return currentState == gpio.HIGH and self.prevState == gpio.LOW
+
+leds = (Led(16, "RED"), Led(21,"Green"))
+
+def ledRedFuncthion():
+    leds[0].blink(10, 0.5)
+
+greenLedState = False
+def ledGreenFuncthion():
+    global greenLedState
+    if greenLedState:
+        leds[1].ledOn()
+    else:
+        leds[1].ledOff()
+    greenLedState = not greenLedState
+
+
+
+buttons = (Button(13, ledRedFuncthion), Button(19, ledGreenFuncthion))
+
+try:
+    while True:
+        for button in buttons:
+            button.waitPressed()
+
+except KeyboardInterrupt:
+    pass
+finally:
+    gpio.cleanup()
